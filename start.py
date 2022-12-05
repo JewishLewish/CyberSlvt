9a8e5fb31ec654837cd8c17f263cbe528059fbc4
JewishLewish <lanbrokhman@gmail.com>
2022-12-05 13:06:46 -0800

    Adjustments to code for if statements to run more smoothly. Error still consist.

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
-import shelter
+import shelter2
 
 while True:
     text = input('>>>')
 
     if text != "":
-        result = shelter.run(text)
+        result = shelter2.run(text)
         if result == None:
             continue
         elif result == "You'd fit perfectly to me and we'd end our loneliness.":
